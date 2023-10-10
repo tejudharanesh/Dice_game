bf5b8e0b79987bbf721099499ed1e647344a7442
teju_d Github <tejudharanesh1234@gmail.com>
2023-10-10 11:06:48 +0530

    final commit

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-// 
+document.querySelector("h3").innerText ="DRAW";
+document.getElementById("player1").addEventListener("click",result);
+document.getElementById("player2").addEventListener("click",result);
 
+function result(){
+    var player1;
+    var player2;
 
+    player1 = Math.floor((Math.random() * 6) + 1);
+    player2 = Math.floor((Math.random() * 6) + 1);
 
+    document.querySelectorAll("img")[0].setAttribute("src","images/"+player1+".png");
+    document.querySelectorAll("img")[1].setAttribute("src","images/"+player2+".png");
 
-window.addEventListener('load', function (event) {
-var player1;
-var player2;
+    if(player1 > player2){
+        document.querySelector("h3").innerText ="Player 1 won";
+    }
+    else if(player2 > player1){
+        document.querySelector("h3").innerText ="Player 2 won";
+    }
+    else{
+        document.querySelector("h3").innerText ="DRAW";
 
-player1 = Math.floor((Math.random() * 6) + 1);
-player2 = Math.floor((Math.random() * 6) + 1);
-
-document.querySelectorAll("img")[0].setAttribute("src","images/"+player1+".png");
-document.querySelectorAll("img")[1].setAttribute("src","images/"+player2+".png");
-
-if(player1 > player2){
-    document.querySelector("h3").innerText ="Player 1 won";
+    }
 }
-else if(player2 > player1){
-    document.querySelector("h3").innerText ="Player 2 won";
-}
-else{
-    document.querySelector("h3").innerText ="DRAW";
-
-}
-});
